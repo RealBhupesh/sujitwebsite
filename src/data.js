3953f1f7a5e8ae8055bc2594a1d5723ed6528065
Bhupesh Pravin Cholake <76155786+RealBhupesh@users.noreply.github.com>
2023-06-25 19:42:35 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Custom Website",
+    subtitle: "MERN Stack",
+    description:
+      "Our engineers have built this website for a furniture store",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Digital Marketing",
+    subtitle: "Facebook ads and Google ads",
+    description:
+      "We ran ads for an audio streaming platform",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "Our Custom Messaging app",
+    description:
+      "We built a custom blockchain based messaging app for our clients so that their personal remain safe.",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Made a Epic To-Do app",
+    subtitle: "Ethereum (dApps)",
+    description:
+      "Made a Epic interconnected To-do Blockchain based app",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Shield Grow, led by Sujit Gaware, delivered outstanding results for our business. Their tailored strategies and expert guidance drove remarkable growth and improved our brand visibility. Highly recommended!",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "DEOLA.Ai",
+  },
+  {
+    quote:
+      "We are extremely pleased with the marketing services provided by Shield Grow. Under the leadership of Sujit Gaware, their team delivered exceptional results, boosting our brand visibility and driving significant business growth. Their expertise and dedication surpassed our expectations. Highly recommended!",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Bender Sandos",
+    company: "BadIdea.IO",
+  },
+];
+
+export const skills = [
+  "Facebook Ads",
+  "Google Ads ",
+  "Web development",
+  "Online Business Development",
+  "Tech Business Traning",
+  "Consultancy",
+];
